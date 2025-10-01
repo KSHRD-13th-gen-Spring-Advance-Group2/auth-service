5aa65d01bbffb133c2e7f2651f2ca8e7759d5714
Lyhengsun <hengsun2.168@gmail.com>
2025-10-01 14:57:45 +0700

    added validation to request

--- src/main/java/com/kshrd/auth_service/model/dto/request/RefreshTokenRequest.java
+++ src/main/java/com/kshrd/auth_service/model/dto/request/RefreshTokenRequest.java
@@ -1,5 +1,6 @@
 package com.kshrd.auth_service.model.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RefreshTokenRequest {
+    @NotBlank(message = "Refresh Token is required")
     private String refreshToken;
 }
--- src/main/java/com/kshrd/auth_service/model/dto/request/UpdateUserRequest.java
+++ src/main/java/com/kshrd/auth_service/model/dto/request/UpdateUserRequest.java
@@ -1,5 +1,8 @@
 package com.kshrd.auth_service.model.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +13,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class UpdateUserRequest {
+
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
+    @Pattern(
+            regexp = "^[A-Za-zÀ-ÖØ-öø-ÿ]+([ '-][A-Za-zÀ-ÖØ-öø-ÿ]+)*$",
+            message = "First name must contain only letters, spaces, apostrophes, or hyphens"
+    )
     private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
+    @Pattern(
+            regexp = "^[A-Za-zÀ-ÖØ-öø-ÿ]+([ '-][A-Za-zÀ-ÖØ-öø-ÿ]+)*$",
+            message = "Last name must contain only letters, spaces, apostrophes, or hyphens"
+    )
     private String lastName;
     private String imageUrl;
 }
